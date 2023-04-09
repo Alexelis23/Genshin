81cb74c9d37024e338b6d1794a41fbc536080ce0
Alexelis23 <eliseeva.alexandra.99@mail.ru>
2023-04-09 23:04:57 +0400

    add name and label

--- app/src/main/java/com/example/genshin/MainActivity.kt
+++ app/src/main/java/com/example/genshin/MainActivity.kt
@@ -3,11 +3,21 @@ package com.example.genshin
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.genshin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.txtView.text = "my text"
+        val count: Int = 10
+        binding.txtView.text = count.toString()
+        var isClicked: Boolean = false
+
     }
 }
